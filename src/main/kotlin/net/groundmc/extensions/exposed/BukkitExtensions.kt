6ac2b8f2e051912eaf6424338bfa5f2e1dffa8f2
Marvin <marv.1997@live.de>
2018-09-09 01:56:46 +0200

    Properly serialize Locations by using their serialize function, turning them into Maps

--- src/main/kotlin/net/groundmc/extensions/exposed/BukkitExtensions.kt
+++ src/main/kotlin/net/groundmc/extensions/exposed/BukkitExtensions.kt
@@ -1,15 +1,15 @@
 package net.groundmc.extensions.exposed
 
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import net.groundmc.extensions.gson.DateTimeAdapter
-import net.groundmc.extensions.gson.LocationTypeAdapter
 import org.bukkit.Location
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.VarCharColumnType
 import org.joda.time.DateTime
+import java.lang.reflect.Type
 
 private val gson = GsonBuilder()
-        .registerTypeAdapter(Location::class.java, LocationTypeAdapter)
         .registerTypeAdapter(DateTime::class.java, DateTimeAdapter)
         .create()
 
@@ -31,23 +31,25 @@ fun Table.location(name: String) = registerColumn<Location>(name, LocationColumn
  */
 class LocationColumnType : VarCharColumnType(255) {
 
+    private val mapStringAnyType: Type = TypeToken.getParameterized(Map::class.java, String::class.java, Any::class.java).type
+
     override fun nonNullValueToString(value: Any): String {
         return when (value) {
-            is Location -> gson.toJson(value)
+            is Location -> gson.toJson(value.serialize())
             else -> super.nonNullValueToString(value)
         }
     }
 
     override fun notNullValueToDB(value: Any): Any {
         return when (value) {
-            is Location -> gson.toJson(value)
+            is Location -> gson.toJson(value.serialize())
             else -> super.notNullValueToDB(value)
         }
     }
 
     override fun valueFromDB(value: Any): Any {
         return when (value) {
-            is String -> gson.fromJson(value, Location::class.java)
+            is String -> Location.deserialize(gson.fromJson(value, mapStringAnyType))
             else -> super.valueFromDB(value)
         }
     }
--- src/main/kotlin/net/groundmc/extensions/gson/BukkitAdapters.kt
+++ src/main/kotlin/net/groundmc/extensions/gson/BukkitAdapters.kt
@@ -1,52 +1,10 @@
 package net.groundmc.extensions.gson
 
-import com.google.gson.GsonBuilder
 import com.google.gson.TypeAdapter
-import com.google.gson.reflect.TypeToken
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
 import org.bukkit.Location
 import org.joda.time.DateTime
-import java.lang.reflect.Type
-
-private val gson = GsonBuilder().create()
-
-/**
- * An adapter to serialize and deserialize [Location] objects.
- *
- * @author GiantTree
- * @since 1.0
- */
-object LocationTypeAdapter : TypeAdapter<Location>() {
-
-    private val mapStringAnyType: Type = TypeToken.getParameterized(Map::class.java, String::class.java, Any::class.java).type
-
-    /**
-     * Writes a [Location] object to a [JsonWriter].
-     *
-     * @param writer the [JsonWriter] to write to.
-     * @param location the [Location] to write.
-     */
-    override fun write(writer: JsonWriter, location: Location?) {
-        when {
-            location != null -> writer.value(gson.toJson(location.serialize()))
-            else -> writer.nullValue()
-        }
-    }
-
-    /**
-     * Reads a JSON-encoded string and converts it to a [Location] object.
-     *
-     * @param reader the [JsonReader] to read from.
-     *
-     * @return the deserialized [Location] object
-     */
-    override fun read(reader: JsonReader): Location? {
-        val read = reader.nextString()
-
-        return Location.deserialize(gson.fromJson<Map<String, Any>>(read, mapStringAnyType))
-    }
-}
 
 /**
  * An adapter to serialize and deserialize [DateTime] objects.
